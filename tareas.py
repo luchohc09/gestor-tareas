31961c1e2041051e14ef2ffceb0b1af137446a29
luchohc09 <luchohc100@gmail.com>
2025-09-19 07:38:43 -0500

    Update tareas.py

--- tareas.py
+++ tareas.py
@@ -26,13 +26,14 @@ if not st.session_state.logged_in:
     password = st.text_input("Contraseña", type="password")
 
     if st.button("Ingresar"):
-        if usuario in USUARIOS and password == USUARIOS[usuario]["password"]:
-            st.session_state.logged_in = True
-            st.session_state.usuario = usuario
-            st.success("Bienvenido!")
-            #st.rerun()
-        else:
-            st.error("❌ Usuario o contraseña incorrectos")
+    if usuario in USUARIOS and password == USUARIOS[usuario]["password"]:
+        st.session_state.logged_in = True
+        st.session_state.usuario = usuario
+        st.success(f"Bienvenido {usuario} 👋")
+        st.experimental_set_query_params(logged="true")
+    else:
+        st.error("❌ Usuario o contraseña incorrectos")
+
 
 # =======================
 # 3. Mostrar GESTOR solo si hay login
@@ -59,4 +60,5 @@ else:
             json.dump(st.session_state.tareas, f, ensure_ascii=False, indent=4)
 
     
+
 
